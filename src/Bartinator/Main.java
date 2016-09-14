5b3f8c4e7de52a1ad32287dbdd33fbfa94cf5d2e
MadsChris92 <madschris888@gmail.com>
2016-09-14 13:43:12 +0200

    scene change

--- src/Bartinator/Main.java
+++ src/Bartinator/Main.java
@@ -1,16 +1,19 @@
 package Bartinator;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     static Stage theStage;
-
+    Button b;
     @Override
     public void start(Stage primaryStage) throws Exception{
         theStage = primaryStage;
@@ -18,9 +21,23 @@ public class Main extends Application {
         theStage.setTitle("Bartinator");
         theStage.setScene(new Scene(root, 700, 600));
         primaryStage.show();
-        Button b = (Button) root.lookup("#loginBtn");
+        b = (Button) root.lookup("#loginBtn");
+
+        b.setOnAction(e -> {
+            try {
+                loginHandler();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
         b.setText("test");
 
+    }
+
+    private void loginHandler() throws IOException {
+        Parent root1 = FXMLLoader.load(getClass().getResource("View/editor.fxml"));
+        theStage.setScene(new Scene(root1, 700, 600));
+ //       b.setText("dasda");
     }
 
 
