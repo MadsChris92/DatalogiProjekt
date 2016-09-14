d8619393d6d823410e61a285289599ae52774e5f
MadsChris92 <madschris888@gmail.com>
2016-09-14 16:25:11 +0200

    scene change

---
+++ src/Bartinator/Controller/editorController.java
@@ -0,0 +1,17 @@
+package Bartinator.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+/**
+ * Created by Gamer on 14-09-2016.
+ */
+public class editorController {
+
+    public Button addItemBtn;
+
+
+    public void addItemHandler(ActionEvent actionEvent) {
+        System.out.println("dasda");
+    }
+}
