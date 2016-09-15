17fe9b75169c72408bf3829e09b33f096d2d0fdb
MadsChris92 <madschris888@gmail.com>
2016-09-15 13:35:45 +0200

    btncreate

--- src/Bartinator/Controller/editorController.java
+++ src/Bartinator/Controller/editorController.java
@@ -2,6 +2,9 @@ package Bartinator.Controller;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+
+import java.util.ArrayList;
 
 /**
  * Created by Gamer on 14-09-2016.
@@ -9,9 +12,21 @@ import javafx.scene.control.Button;
 public class editorController {
 
     public Button addItemBtn;
-
+    public VBox btnContainer;
+    public Button removeItemBtn;
+    int btnCount = 0;
+    ArrayList<Button> btnList = new ArrayList<Button>();
 
     public void addItemHandler(ActionEvent actionEvent) {
-        System.out.println("dasda");
+        Button b = new Button("hallo manner");
+        btnContainer.getChildren().add(b);
+        btnList.add(b);
+        setBtnPreference();
+    }
+
+    public void setBtnPreference(){
+        for (int i = 0; i < btnList.size(); i++) {
+            btnList.get(i).setMinSize(100,100);
+        }
     }
 }
