18122957026993cf1909affeda3b0b72d240c58c
danielbjorkholm <daniel.bjorkholm@me.com>
2016-09-15 14:15:41 +0200

    MVC added

--- src/Bartinator/Controller/loginController.java
+++ src/Bartinator/Controller/loginController.java
@@ -16,13 +16,13 @@ import java.io.IOException;
 
 public class loginController {
 
-    public Button loginBtn;
+    public Button adminLoginBtn;
     public Label feedackField;
     public TextField usernameField;
     public PasswordField passwordField;
 
 
-    public void handleLogin(ActionEvent actionEvent) {
+    public void handleAdminLogin(ActionEvent actionEvent) {
         System.out.println("o/");
         if(verifyLogin(usernameField.getText(), passwordField.getText())){
             System.out.println("login good");
