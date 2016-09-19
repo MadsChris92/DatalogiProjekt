f67b63a17103299885453d781c595aa3a01c2250
MadsChris92 <madschris888@gmail.com>
2016-09-19 12:33:56 +0200

    scene change

--- src/Bartinator/Controller/editorController.java
+++ src/Bartinator/Controller/editorController.java
@@ -1,34 +1,55 @@
 package Bartinator.Controller;
 
+import Bartinator.Other.ButtonCustom;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
  * Created by Gamer on 14-09-2016.
  */
 public class editorController {
-
+    Stage popUpStage;
     public Button addItemBtn;
     public VBox btnContainer;
     public Button removeItemBtn;
     public Label txtHeader;
     int btnCount = 0;
+    Parent root1 = null;
     ArrayList<Button> btnList = new ArrayList<Button>();
 
     public void addItemHandler(ActionEvent actionEvent) {
-        Button b = new Button("hallo manner");
+        ButtonCustom b = new ButtonCustom();
         b.setOnAction(e -> itemHandler());
         btnContainer.getChildren().add(b);
         btnList.add(b);
         setBtnPreference();
+        startPopUp();
     }
 
     private void itemHandler() {
         System.out.println("fads");
+    }
+
+    public void startPopUp(){
+        try {
+            root1 = FXMLLoader.load(getClass().getResource("../View/btnCreate.fxml"));
+            System.out.println("dada");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        popUpStage.setScene(new Scene(root1, 500, 500));
+        System.out.println("d");
+        popUpStage.initModality(Modality.APPLICATION_MODAL);
     }
 
     public void setBtnPreference(){
---
+++ src/Bartinator/Other/ButtonCustom.java
@@ -0,0 +1,27 @@
+package Bartinator.Other;
+
+import javafx.scene.control.Button;
+
+/**
+ * Created by Gamer on 19-09-2016.
+ */
+public class ButtonCustom extends Button{
+    String name;
+    float price;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+}
