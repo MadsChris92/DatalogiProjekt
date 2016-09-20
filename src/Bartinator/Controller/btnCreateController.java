f2fd13011cf05ea254dd41d5bed239848a937ad0
MadsChris92 <madschris888@gmail.com>
2016-09-20 14:53:00 +0200

    btn create stage cahnges

--- src/Bartinator/Controller/btnCreateController.java
+++ src/Bartinator/Controller/btnCreateController.java
@@ -4,27 +4,33 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 
 public class btnCreateController {
 
-    public Button btn;
     public Label txt;
     public Button btnDescription;
     public VBox descripContainer;
 
+    public void addDesciption(MouseEvent mouseEvent) {
 
-    public void applyChanges(MouseEvent mouseEvent) {
-        System.out.println("test");
-        txt.setText("dasdas");
+        // TODO: Label tekst skal kunne defineres af brugeren + information skal sendes til database
+        Label l = new Label("_________________________________");
+        TextField t = new TextField("Enter description here!");
+        Button b = new Button("✓");
+        Button b1 = new Button("+");
+        Label l1 = new Label("");
+        HBox container = new HBox();
+
+        descripContainer.getChildren().addAll(l,container,t);
+        container.getChildren().addAll(b1,b);
+        b1.setOnAction(event -> addParameter());
     }
 
-    public void addDesciption(MouseEvent mouseEvent) {
-        Label l = new Label("Test");
-        TextField t = new TextField("Enter description here!");
-        Button b = new Button("Add");
-        Label l1 = new Label("");
-        descripContainer.getChildren().addAll(l,t,b,l1);
+    private void addParameter() {
+        TextField t1 = new TextField("Enter description here!");
+        descripContainer.getChildren().add(t1);
     }
 }
