3cb7e5cdad35ec5bc155fad91ec162ff8cb85bc4
MadsChris92 <madschris888@gmail.com>
2016-11-02 14:50:11 +0100

    dsa

--- src/Bartinator/DataAccessObjects/ProductDataAccessObject.java
+++ src/Bartinator/DataAccessObjects/ProductDataAccessObject.java
@@ -62,4 +62,9 @@ public class ProductDataAccessObject extends MainDataAccessObject {
 		remove(product);
 		save(product);
 	}
+
+
+    public void updateCategory(Category activeCategory) {
+        save(activeCategory);
+    }
 }
